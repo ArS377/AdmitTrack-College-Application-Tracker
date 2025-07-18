ef95d285cfe132bf0f520fc2643abce866cb0ae1
ArS377 <arysom992@gmail.com>
2025-07-18 00:25:45 -0700

    mycollege page css changes

--- college-app/src/components/CollegeList.jsx
+++ college-app/src/components/CollegeList.jsx
@@ -5,29 +5,31 @@ const CollegeList = ({ collegeList, deleteCollegeFromList }) => {
   return (
     <>
       <h2>My College List</h2>
-      <div>
-        {collegeList.length > 0 ? (
-          collegeList.map((college) => (
-            <div key={college._id}>
-              <p>
-                {college.collegeName}
-                <button
-                  className="btn btn-danger"
-                  onClick={() =>
-                    deleteCollegeFromList(college._id, getUser()?.email)
-                  }
-                >
-                  X
-                </button>
-              </p>
-              <div className="dropdown-menu">
-                <Dropdown />
+      <div className="list-group">
+        <div class="d-grid gap-3">
+          {collegeList.length > 0 ? (
+            collegeList.map((college) => (
+              <div className="p-2 bg-light border" key={college._id}>
+                <p>
+                  {college.collegeName}
+                  <button
+                    className="btn btn-danger"
+                    onClick={() =>
+                      deleteCollegeFromList(college._id, getUser()?.email)
+                    }
+                  >
+                    X
+                  </button>
+                </p>
+                <div className="dropdown-menu">
+                  <Dropdown />
+                </div>
               </div>
-            </div>
-          ))
-        ) : (
-          <p>Search for colleges to add them to your list.</p>
-        )}
+            ))
+          ) : (
+            <p>Search for colleges to add them to your list.</p>
+          )}
+        </div>
       </div>
     </>
   );
