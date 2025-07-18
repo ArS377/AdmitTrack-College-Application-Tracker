0eeedacc7e147f6123e2ae170a2d5921401ea076
ArS377 <arysom992@gmail.com>
2025-07-17 18:16:12 -0700

    some changes

--- college-app/src/components/Dropdown.jsx
+++ college-app/src/components/Dropdown.jsx
@@ -1,68 +1,77 @@
-import {useState, useRef, useEffect} from 'react'
-import './Dropdown.css'
+import { useState, useRef, useEffect } from "react";
+import "./Dropdown.css";
 
 const Dropdown = () => {
-    const [dropdownToggled, setDropdownToggled] = useState(false)
-    const dropdownRef = useRef(null)
-    const [selectedOption, setSelectedOption] = useState(null)
+  const [dropdownToggled, setDropdownToggled] = useState(false);
+  const dropdownRef = useRef(null);
+  const [selectedOption, setSelectedOption] = useState(null);
 
-    useEffect(() => {
-        function handler(e) {
-            if (dropdownRef.current) {
-                if (!dropdownRef.current.contains(e.target)) {
-                    setDropdownToggled(false)
-                }
-            }
+  useEffect(() => {
+    function handler(e) {
+      if (dropdownRef.current) {
+        if (!dropdownRef.current.contains(e.target)) {
+          setDropdownToggled(false);
         }
-        document.addEventListener('click', handler)
+      }
+    }
+    document.addEventListener("click", handler);
 
-        return () => {
-            document.removeEventListener('click', handler)
-        }
-    })
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  });
 
-    const dropdownOptions = [
-        {
-            id: 1,
-            label: 'Dream'
-        },
-        {
-            id: 2,
-            label: 'Reach'
-        },
-        {
-            id: 3,
-            label: 'Target'
-        },
-        {
-            id: 4,
-            label: 'Safety'
-        }
-    ]
-    return (
-        <div className='dropdown' ref={dropdownRef}>
-            <button className='toggle' onClick={() => {
-                setDropdownToggled(!dropdownToggled)
-            }}>
-                <span>{selectedOption ? selectedOption.label : 'Select Category'}</span>
-                <span>{dropdownToggled ? '∧' : '∨'}</span>
+  const dropdownOptions = [
+    {
+      id: 1,
+      label: "Dream",
+    },
+    {
+      id: 2,
+      label: "Reach",
+    },
+    {
+      id: 3,
+      label: "Target",
+    },
+    {
+      id: 4,
+      label: "Safety",
+    },
+  ];
+  return (
+    <div className="dropdown" ref={dropdownRef}>
+      <button
+        className="toggle"
+        onClick={() => {
+          setDropdownToggled(!dropdownToggled);
+        }}
+      >
+        <span>{selectedOption ? selectedOption.label : "Select Category"}</span>
+        <span>{dropdownToggled ? "∧" : "∨"}</span>
+      </button>
+      <div className={`options ${dropdownToggled ? "visible" : ""}`}>
+        {dropdownOptions.map((option, index) => {
+          return (
+            <button
+              key={option.id}
+              className={`${
+                selectedOption && selectedOption.id === option.id
+                  ? "selected"
+                  : ""
+              }`}
+              onClick={() => {
+                setSelectedOption(option);
+                setDropdownToggled(false);
+              }}
+            >
+              {option.label}
             </button>
-            <div className={`options ${dropdownToggled ? 'visible' : ''}`}>
-                {dropdownOptions.map((option, index) => {
-                    return (
-                        <button 
-                        className={`${selectedOption && selectedOption.id === option.id ? "selected" : ""}`} 
-                        onClick={()=>{
-                            setSelectedOption(option)
-                            setDropdownToggled(false)
-                        }}>
-                            {option.label}
-                        </button>
-                    )
-                })}
-            </div>
-        </div>
-    )
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Dropdown
+export default Dropdown;
