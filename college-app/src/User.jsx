aeb9239a56997d70a7d48be8d5d22e5691d08264
soma-ellappan <33524746+soma-ellappan@users.noreply.github.com>
2025-08-04 21:51:35 -0700

    tab refresh now maintains the session.

--- college-app/src/User.jsx
+++ college-app/src/User.jsx
@@ -13,27 +13,29 @@ export function getUser() {
   return user;
 }
 
-const validatedAccessToken = async (token) => {
+const isAccessTokenValid = async (token) => {
   try {
     const response = await axios.post(
       "http://localhost:3000/api/auth/validate",
+      {},
       {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       }
     );
-    if (response.status === 200) {
-      console.log("Access token is valid.");
-      return token;
+    if (response.status !== 200) {
+      console.warn("Access token is invalid: ", response.statusText);
+      return false;
     }
+    return true;
   } catch (error) {
     console.error("Access token validation failed:", error);
-    return undefined;
+    return false;
   }
 };
 
-export function getAccessToken() {
+export const getAccessToken = () => {
   if (!user.accessToken) {
     // accessToken is not set, try to get it from session storage
     const sessionAccessToken = sessionStorage.getItem("accessToken");
@@ -44,18 +46,19 @@ export function getAccessToken() {
     if (sessionAccessToken) {
       try {
         const token = JSON.parse(sessionAccessToken);
-        user.accessToken = validatedAccessToken(token);
         console.log("getAccessToken: Parsed accessToken:", token);
+        setAccessToken(isAccessTokenValid(token) ? token : undefined);
       } catch (error) {
         console.error("Error parsing accessToken from session storage:", error);
-        user.accessToken = undefined; // Clear if parsing fails
-        sessionStorage.removeItem("accessToken"); // Clear invalid token
+        setAccessToken(undefined); // Clear if parsing fails
       }
+    } else {
+      console.warn("No accessToken found in session storage.");
+      setAccessToken(undefined); // Clear if no token is found
     }
   }
-  console.log("getAccessToken returning:", user.accessToken);
   return user.accessToken;
-}
+};
 
 export function setAccessToken(token) {
   user.accessToken = token;
