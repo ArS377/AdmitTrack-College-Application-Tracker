4d16a70fcfc3203912855f1fa4be1acbdbc267bf
soma-ellappan <33524746+soma-ellappan@users.noreply.github.com>
2025-08-30 06:19:20 -0700

    indicate that the college was added.

--- college-app/src/components/AddThisCollege.jsx
+++ college-app/src/components/AddThisCollege.jsx
@@ -1,7 +1,10 @@
 import { addToMyColleges } from "../utils/collegeUtils";
+import { useState } from "react";
 export default function AddThisCollege({ college, toggleRenderSwitch }) {
+  const [collegeInMyList, setCollegeInMyList] = useState(false);
   console.log("AddThisCollege: ", JSON.stringify(college?.unitId));
   async function handleAddCollege(college) {
+    setCollegeInMyList(true);
     const result = await addToMyColleges(college);
     result && toggleRenderSwitch();
     // TODO the parent component is not refreshing.
@@ -10,17 +13,23 @@ export default function AddThisCollege({ college, toggleRenderSwitch }) {
   }
   return (
     <div>
-      <h4>
-        You have not added this college to your list. If you would like to apply
-        for this college and track the application status here, please add the
-        college to your list of colleges
-      </h4>
-      <button
-        className="btn btn-sm btn-outline-primary"
-        onClick={() => handleAddCollege(college)}
-      >
-        Add
-      </button>
+      {collegeInMyList == false ? (
+        <>
+          <h4>
+            You have not added this college to your list. If you would like to
+            apply for this college and track the application status here, please
+            add the college to your list of colleges
+          </h4>
+          <button
+            className="btn btn-sm btn-outline-primary"
+            onClick={() => handleAddCollege(college)}
+          >
+            Add
+          </button>
+        </>
+      ) : (
+        <h4>Great! We added this college to your list.</h4>
+      )}
     </div>
   );
 }
