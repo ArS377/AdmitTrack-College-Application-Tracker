54209543499861ea7fd8c5898993ce12b5e27dfe
soma-ellappan <33524746+soma-ellappan@users.noreply.github.com>
2025-08-30 06:31:16 -0700

    Setting active tab for CSS

--- college-app/src/components/NavBar.jsx
+++ college-app/src/components/NavBar.jsx
@@ -1,23 +1,30 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../utils/axiosConfig";
 import "./NavBar.css";
 
 function NavBar() {
   const navigate = useNavigate();
+  const [activeTab, setActiveTab] = useState("Home");
 
+  console.log("ActiveTab: ", activeTab);
   const goToHome = () => {
+    setActiveTab("Home");
     navigate("/home");
   };
 
   const goToAddCollege = () => {
+    setActiveTab("Add College");
     navigate("/addcollege");
   };
 
   const goToResearch = () => {
+    setActiveTab("Research");
     navigate("/research");
   };
 
   const goToMyProfile = () => {
+    setActiveTab("Profile");
     navigate("/profile");
   };
 
@@ -33,16 +40,36 @@ function NavBar() {
           <span className="navbar-brand mb-0 h1">College App</span>
           <div className="" id="navbarNav">
             <ul className="navbar-nav">
-              <li className="nav-item nav-link active" onClick={goToHome}>
+              <li
+                className={`nav-item nav-link ${
+                  activeTab === "Home" ? "active" : ""
+                }`}
+                onClick={goToHome}
+              >
                 Home
               </li>
-              <li className="nav-item nav-link" onClick={goToAddCollege}>
+              <li
+                className={`nav-item nav-link ${
+                  activeTab === "Add College" ? "active" : ""
+                }`}
+                onClick={goToAddCollege}
+              >
                 Add College
               </li>
-              <li className="nav-item nav-link" onClick={goToResearch}>
+              <li
+                className={`nav-item nav-link ${
+                  activeTab === "Research" ? "active" : ""
+                }`}
+                onClick={goToResearch}
+              >
                 Research
               </li>
-              <li className="nav-item nav-link" onClick={goToMyProfile}>
+              <li
+                className={`nav-item nav-link ${
+                  activeTab === "Profile" ? "active" : ""
+                }`}
+                onClick={goToMyProfile}
+              >
                 Profile
               </li>
               <li className="nav-item nav-link" onClick={handleSignOut}>
