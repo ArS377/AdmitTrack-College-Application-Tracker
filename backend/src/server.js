d32a2ef06a524f2fe5eb796e851fc8372c3e1942
ArS377 <arysom992@gmail.com>
2026-03-30 23:06:11 -0700

    vercel instructions

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5174"], credentials: true }));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(json());
 app.use(cookieParser()); // Middleware to parse cookies
 
